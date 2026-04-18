5068950644b6c8202169d35775a6caa2d00c0ac1
yazanDarAmmar1 <darammary5@gmail.com>
2026-04-18 21:32:20 +0300

    fixes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,29 @@ use App\Enums\PaymentStatus;
 use App\Models\PaymentTransaction;
 use Illuminate\Support\Facades\Route;
 
+Route::match(['get', 'post'], 'callback', function () {
+
+    $globalTransactionId = request('globalTransactionsId');
+
+    if (!$globalTransactionId) {
+        return redirect()->route('home');
+    }
+
+    $transaction = PaymentTransaction::where('global_transaction_id', $globalTransactionId)->first();
+
+    if (!$transaction) {
+        return redirect()->route('home');
+    }
+
+    if ($transaction->status?->value === PaymentStatus::Paid->value) {
+        return redirect()->route('success-payment', $transaction->no);
+    }
+
+    return redirect()->route('failed-payment', $transaction->no);
+
+})->name('payment.callback');
+
+
 Route::fallback(function () {
     $locale = session('locale', 'ar');
     $path = request()->path();
@@ -16,21 +39,3 @@ Route::fallback(function () {
     return redirect("/{$locale}");
 });
 
-
-Route::get('/callback', function () {
-    $globalTransactionId = request('globalTransactionsId');
-    info($globalTransactionId);
-    if ($globalTransactionId) {
-        $transaction = PaymentTransaction::where('global_transaction_id', $globalTransactionId)->first();
-        info($transaction);
-        if ($transaction) {
-            sleep(3);
-            if ($transaction->status->value === PaymentStatus::Paid->value) {
-                return redirect()->route('success-payment', $transaction->no);
-            }
-            return redirect()->route('failed-payment', $transaction->no);
-        }
-    }
-
-    return redirect()->route('home');
-})->name('payment.callback');
