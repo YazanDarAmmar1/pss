f123da71cb3f7d5ebfef617266ecc7f02d2b9be5
yazanDarAmmar1 <darammary5@gmail.com>
2026-04-18 21:45:38 +0300

    fixes

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,19 @@ use Illuminate\Support\Facades\Route;
 Route::match(['get', 'post'], 'callback', function () {
 
     $globalTransactionId = request('globalTransactionsId');
+    info($globalTransactionId);
 
     if (!$globalTransactionId) {
         return redirect()->route('home');
     }
 
     $transaction = PaymentTransaction::where('global_transaction_id', $globalTransactionId)->first();
+    info($transaction);
 
     if (!$transaction) {
         return redirect()->route('home');
     }
+    info($transaction->status?->value);
 
     if ($transaction->status?->value === PaymentStatus::Paid->value) {
         return redirect()->route('success-payment', $transaction->no);
