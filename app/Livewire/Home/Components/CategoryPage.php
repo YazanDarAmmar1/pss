c6427d5264a376173163ad9e7dfe3c13f61a3774
yazanDarAmmar1 <darammary5@gmail.com>
2026-04-18 21:09:48 +0300

    fixes

--- app/Livewire/Home/Components/CategoryPage.php
+++ app/Livewire/Home/Components/CategoryPage.php
@@ -10,7 +10,7 @@ class CategoryPage extends Component
     public $categories;
     public function mount()
     {
-        $this->categories = Category::active()->get();
+        $this->categories = Category::active()->limit(4)->get();
     }
     public function render()
     {
